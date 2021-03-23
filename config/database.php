c421fd15d399cdf152103b7713309bb613ad0bf7
Harsh Bhatt <bhattharsh5365@yahoo.com>
2021-03-23 20:49:42 +0530

    New settings for deplyment

--- config/database.php
+++ config/database.php
@@ -93,17 +93,7 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('MONGO_DB_HOST', 'localhost'),
-            'port' => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE', 'LinkMe'),
-            'username' => env('MONGO_DB_USERNAME'),
-            'password' => env('MONGO_DB_PASSWORD'),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
+            'dsn'=>'mongodb+srv://'.env('MONGO_DB_USERNAME', '').':'.env('MONGO_DB_PASSWORD', '').'@'.env('MONGO_HOSTS', '127.0.0.1').'/'.env('MONGO_DB_DATABASE', 'LinkMe').'?retryWrites=true&w=majority',
         ],
 
     ],
