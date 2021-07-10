794dee642e60ee913b891430898ca6b678765b1d
Harsh Bhatt <bhattharsh5365@yahoo.com>
2021-07-10 11:36:07 +0530

    added missing parameters

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'active', 'verify_account'
+        'name', 'email', 'password', 'active', 'verify_account', 'first_login', 'user_type_id', 'username'
     ];
 
     /**
